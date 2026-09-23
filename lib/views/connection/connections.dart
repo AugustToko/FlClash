6d4b838cdddb81e2d00b33fae2d48132e7eae963
GiKode <827266641@qq.com>
2026-09-23 20:46:22 +0800

    feat: expose runtime rule priority controls on connections

--- lib/views/connection/connections.dart
+++ lib/views/connection/connections.dart
@@ -31,7 +31,7 @@ class _ConnectionsViewState extends ConsumerState<ConnectionsView>
 
   List<Widget> _buildActions() {
     return [
-      const QuickRoutingRulesButton(),
+      const QuickRoutingRuleManagerButton(),
       IconButton(
         tooltip: context.appLocalizations.closeConnections,
         onPressed: () async {
