6e277462befc80cff97cc7b13120fdbf2585e0f1
GiKode <827266641@qq.com>
2026-09-23 23:15:10 +0800

    feat: wire core rule match into quick routing

--- lib/features/connection/quick_routing.dart
+++ lib/features/connection/quick_routing.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:io';
 
 import 'package:fl_clash/common/common.dart';
+import 'package:fl_clash/core/controller.dart';
 import 'package:fl_clash/database/database.dart';
 import 'package:fl_clash/enum/enum.dart';
 import 'package:fl_clash/models/models.dart';
@@ -12,6 +13,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:material_ui/material_ui.dart';
 
 part 'quick_routing/actions.dart';
+part 'quick_routing/core_match.dart';
 part 'quick_routing/editor.dart';
 part 'quick_routing/explanation.dart';
 part 'quick_routing/logic.dart';
