9342927e290e7bba56f859db81a01c833beaa682
GiKode <827266641@qq.com>
2026-09-24 14:37:32 +0800

    feat(ui): open diagnostics workbench from requests

--- lib/views/connection/requests.dart
+++ lib/views/connection/requests.dart
@@ -54,7 +54,7 @@ class _RequestsViewState extends ConsumerState<RequestsView> {
       title: appLocalizations.requests,
       searchState: AppBarSearchState(onSearch: _listController.search),
       onKeywordsUpdate: _listController.updateKeywords,
-      actions: const [QuickRoutingRuleManagerButton()],
+      actions: const [QuickRoutingWorkbenchButton()],
       floatingActionButton: ValueListenableBuilder(
         valueListenable: _listController,
         builder: (_, state, _) {
