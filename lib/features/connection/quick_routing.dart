c07ea8093a58458e8cdf777b31e5530460869549
GiKode <827266641@qq.com>
2026-09-24 14:38:20 +0800

    fix(ui): import workbench route type explicitly

--- lib/features/connection/quick_routing.dart
+++ lib/features/connection/quick_routing.dart
@@ -9,6 +9,7 @@ import 'package:fl_clash/enum/enum.dart';
 import 'package:fl_clash/models/models.dart';
 import 'package:fl_clash/providers/providers.dart';
 import 'package:fl_clash/widgets/widgets.dart';
+import 'package:flutter/material.dart' show MaterialPageRoute;
 import 'package:flutter/widgets.dart' show WidgetsLocalizations;
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:material_ui/material_ui.dart';
